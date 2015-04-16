2cdcd8dcdf72b2d3fec2121760f7a7fb2c62ba05
amitabha <amitabha.yin@foxmail.com>
2015-04-16 10:14:31 +0800

    add more tests and assertions

--- my-max-test.lisp
+++ my-max-test.lisp
@@ -1,2 +1,21 @@
 (define-test test-my-max
-  (assert-equal 5 (my-max 2 5)))
+  (assert-equal 5 (my-max 2 5))
+  (assert-equal 4 (my-max 1 4)))
+
+(define-test negative-test-my-max
+  (assert-equal 3 (my-max 3 5)))
+
+(define-test failure-test-my-max
+  (assert-equal 2 (my-max 3 2)))
+
+(define-test assert-true-test
+  (assert-true t))
+
+(define-test assert-true-test
+  (assert-true nil))
+
+(defun my-sqrt (n) (/ n 2))
+
+(define-test test-loop-assertion
+  (dotimes (i 5)
+    (assert-equal i (my-sqrt (* i i)) i)))
--- run-my-max-tests.lisp
+++ run-my-max-tests.lisp
@@ -2,8 +2,12 @@
 
 (use-package :lisp-unit)
 
+(setq *print-failures* t)
+
 (load "my-max")
 
 (load "my-max-test")
 
 (run-tests)
+
+(print-errors *)
